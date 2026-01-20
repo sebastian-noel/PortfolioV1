08f2980ce6ce845c42f33c4e2c446331639ead62
Sebastian Noel <snoel.dev@gmail.com>
2026-01-19 23:06:47 -0500

    replaced typing test sentences

--- src/data/typingTest.ts
+++ src/data/typingTest.ts
@@ -4,18 +4,11 @@ export const typingTestConfig = {
 
   // Edit premade sentences 
   sentences: [
-    "I build custom mechanical keyboards and love the sound of lubed switches.",
-    "Software engineering is my passion, from research to web development.",
-    "I study Computer Science at the University of Central Florida in Orlando.",
-    "My favorite part of coding is solving complex problems with elegant solutions.",
-    "When I am not coding you can find me tweaking my keyboard or gaming setup.",
-    "I believe that the right peripherals can transform your computing experience.",
-    "There is something satisfying about a perfectly tuned mechanical keyboard.",
-
+    "i study computer science with a minor in intelligent robotic systems at the university of central florida",
     "the first hackathon i ever attended was knight hacks vii during my freshman year where i built dish dash",
     "my favorite hackathon project i worked on was tidesense where i worked with stevin, nicole, and chris",
     "i am one of the founding officers of graphics programming knights and currently serve as the secretary",
-    ""
-
-  ],
+    "i am a member of both the society of hispanic professional engineers, also known as shpe, and colorstack",
+    "java was the first programming language i learned and still use it today for coursework like computer science ii",
+    ],
 };
